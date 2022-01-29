d14094b5c2a7f5c9497ee7eea53278d9ac343ee0
nely de faria <nelly.nf54@gmail.com>
2022-01-29 18:08:17 -0300

    Criando arquivo operacoes_matematicas.py

---
+++ curso_python/modulo_01/operacoes_matematicas.py
@@ -0,0 +1,24 @@
+import math
+
+n1 = 5
+n2 = 3
+
+print(f'A soma de {n1} + {n2} é {n1 + n2}')
+
+print(f'A subtração de {n1} - {n2} é {n1 - n2}')
+
+print(f'A divisão de {n1} / {n2} é {n1 / n2}')
+
+print(f'A multiplicação de {n1} * {n2} é {n1 * n2}')
+
+print(f'O resto da divisão de {n1} por {n2} é {n1 % n2}')
+
+print('5 elevado a 3 é', 5**3)
+
+print(math.sqrt(81))
+
+casos_doenca = 134
+numero_habitantes = 34432
+casos_por_habitante = casos_doenca / numero_habitantes
+print(casos_por_habitante)
+print('O número de casos por habitante é de', round(casos_por_habitante, 6))
