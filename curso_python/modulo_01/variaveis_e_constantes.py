a00bd4d5a335e35ad9ade1b003755b58e378c9a2
nely de faria <nelly.nf54@gmail.com>
2022-01-29 17:29:45 -0300

    Criando arquivo variaveis_e_constantes.py

---
+++ curso_python/modulo_01/variaveis_e_constantes.py
@@ -0,0 +1,31 @@
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+
+# print(numero, numero_jogos, numero_convidados)
+
+PI = 3.14
+NUMERO_EULER = 2.71
+ESCALA_TERREMOTO = -4.55
+
+print(PI)
+print(NUMERO_EULER)
+print(ESCALA_TERREMOTO)
+
+letra = 'a'
+palavra1 = 'linguagem de programação'
+palavra2 = 'Python'
+
+# print('Estou aprendendo uma', palavra1)
+# print('Esta', palavra1, 'se chama', palavra2)
+
+# idade = int(input('Digite sua idade: '))
+# print('Sua idade é', idade)
+# print(type(idade))
+
+ph = float(input('Qual o ph do solo durante a última medição?'))
+print('O ph medido foi', ph)
+print(type(ph))
+
+# nome = input('Qual o seu nome?')
+# print('Seu nome é', nome)
