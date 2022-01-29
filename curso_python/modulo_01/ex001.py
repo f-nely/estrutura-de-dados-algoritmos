0a7c6356904dc7b380328aa7f93f8e59a9b68bcd
nely de faria <nelly.nf54@gmail.com>
2022-01-29 18:08:43 -0300

    Criando arquivo ex001.py

---
+++ curso_python/modulo_01/ex001.py
@@ -0,0 +1,14 @@
+num1 = int(input('Informe o num1: '))
+num2 = int(input('Informe o num2: '))
+
+print(f'A soma entre {num1} e {num2} é {num1 + num2}')
+
+print(f'A subtração entre {num1} e {num2} é {num1 - num2}')
+
+div = num1 / num2
+divisao = round(div, 1)
+# print(divisao)
+
+print(f'A divisão entre {num1} e {num2} é {divisao}')
+
+print(f'A multiplicação entre {num1} e {num2} é {num1 * num2}')
