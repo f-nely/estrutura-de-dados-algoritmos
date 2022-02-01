001c2bd3ed7c986935a1bd7a11c51d7f93aaa9f4
nely de faria <nelly.nf54@gmail.com>
2022-02-01 12:00:23 -0300

    Criando arquivo operadores_condicionais.py

---
+++ curso_python/modulo_01/operadores_condicionais.py
@@ -0,0 +1,26 @@
+if 5 > 3:
+    print('5 é maior que 3')
+# print('teste')
+
+if 5 > 6:
+    print('5 é maior que 6')
+else:
+    print('5 não é maior que 6')
+
+n = 9
+
+if n == 4:
+    print('n é igual a 4')
+else:
+    if n == 3:
+        print('n é igual a 3')
+    else:
+        print('n não é igual a 4 nem 3')
+
+x = 2
+y = 4
+
+if (x > 1) and (y % 2 == 0):
+    print('x é maior que 1 e y é par')
+else:
+    print('Uma ou nenhume das condições foram satisfeitas')
