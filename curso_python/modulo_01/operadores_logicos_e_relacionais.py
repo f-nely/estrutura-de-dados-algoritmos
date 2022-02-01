6fa3d1d1689797b71cd4bdc47d1ff3ecc3ee83a5
nely de faria <nelly.nf54@gmail.com>
2022-02-01 11:36:45 -0300

    Criando arquivo operadores_logicos_e_relacionais.py

---
+++ curso_python/modulo_01/operadores_logicos_e_relacionais.py
@@ -0,0 +1,24 @@
+# operadores lógicos
+a = True
+b = False
+
+print(a, b)
+print(a and b)
+print(a & b)
+c = a and b
+print("'A' e 'B' são iguais é:", c)
+
+print(a or b)
+print(a | b)
+d = a or b
+print("'A' ou 'B' são iguais é:", d)
+
+print(not a)
+
+# operadores relacionais
+print(5 > 3)
+print(5 < 3)
+print(5 >= 5)
+print(5 == 3)
+print(5 != 3)
+
