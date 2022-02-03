74603e39165388d9635ccdce669333e59811aad6
nely de faria <nelly.nf54@gmail.com>
2022-02-03 17:08:31 -0300

    Criando arquivo for.py

---
+++ curso_python/modulo_01/for.py
@@ -0,0 +1,32 @@
+for numero in range(1, 6):
+    print(numero)
+
+print()
+
+for numero in range(5, 0, -1):
+    print(numero)
+
+print()
+
+soma = 0
+for numero in range(1, 6):
+    soma += numero
+print(soma)
+
+print()
+
+palavra = 'sorvete'
+for letra in palavra:
+    # print(letra)
+    if letra == 'v':
+        print('Achou a letra v')
+
+print()
+
+for i in range(0, 5):
+    print(i)
+    print('===')
+    for j in range(0, 3):
+        print(j)
+    print()
+
